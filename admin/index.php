e999603f1c33d1847d20318f5b7073fe4e3b316b
Giorrrr <giovanniyuvensetiawan@gmail.com>
2022-09-12 11:57:31 +0800

    redesign navbar

--- admin/index.php
+++ admin/index.php
@@ -19,41 +19,45 @@ if(!isset($_SESSION['email'])  && $_SESSION['status'] != "login") {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>FLEXBOX</title>
-    <link rel="stylesheet" href="../assets/css/style.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap" rel="stylesheet" />
-  </head>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
-  <body>
-    <!-- Sidebar -->
-    <nav>
-      <div class="logo">
-        <h4><?php echo $user['nama']; ?></h4>
-      </div>
-      <ul>
-        <li><a href="index.php" style="color:white; font-weight:500;">Dashboard</a></li>
-        <li><a href="">Book Data</a></li>
-        <li><a href="">Service</a></li>
-        <li><a href="">Gallery</a></li>
-        <li><a href="">About</a></li>
+  <title>Dashboard</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="index.php"><?php echo $user['nama']; ?></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Dashboard</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Master
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="class.php">Class</a></li>
+            <li><a class="dropdown-item" href="student.php">Student</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="bookCategory.php">Book Category</a></li>
+            <li><a class="dropdown-item" href="book.php">Book</a></li>
+          </ul>
+        </li>
       </ul>
+    </div>
+  </div>
+</nav>
 
-      <div class="menu-toggle">
-        <input type="checkbox" />
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </nav>
-    <!-- End Sidebar -->
 
-    <!-- Dashboard -->
 
-    <!-- End Dashboard-->
-    <script src="../assets/js/script.js"></script>
-  </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
 </html>
